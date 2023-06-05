085e069e8ea986a4c85881fa7a4a738b4794f20f
ScreamForceOne <ScreamForceOne@SFO-PC>
2023-06-05 22:19:31 +0200

    buildattempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  calculationResult: string | number = '0';
+
+  onDigitClick(digit: number) {
+    this.calculationResult = this.calculationResult === '0' ? digit.toString() : this.calculationResult.toString() + digit.toString();
+  }
+
+  onOperatorClick(operator: string) {
+    this.calculationResult = operator;
+  }
+
+  calculateResult() {
+    // Implement the logic to calculate the result
+  }
 }
---
+++ src/app/digit/digit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-digit',
+  templateUrl: './digit.component.html',
+  styleUrls: ['./digit.component.css']
+})
+export class DigitComponent {
+
+}
