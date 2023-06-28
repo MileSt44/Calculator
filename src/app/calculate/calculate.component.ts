8048ecbc90164e193181318fa43ba82ec9ff2e32
ScreamForceOne <ScreamForceOne@SFO-PC>
2023-06-28 17:58:52 +0200

    "="

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./calculate.component.css']
 })
 export class CalculateComponent {
-
+  display: string = ''; // Variable to hold the displayed calculation
+  
+  handleButtonClick(value: string): void {
+    if (value === '=') {
+      this.calculateResult();
+    } else {
+      this.display += value;
+    }
+  }
+  
+  calculateResult(): void {
+    try {
+      // Use the eval function to evaluate the calculation string
+      const result = eval(this.display);
+      
+      // Update the display with the calculated result
+      this.display = result.toString();
+    } catch (error) {
+      // Handle any calculation errors
+      console.error('Calculation error:', error);
+    }
+  }
 }
